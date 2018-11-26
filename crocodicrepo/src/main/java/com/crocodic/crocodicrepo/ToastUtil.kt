cd1ca912109c981edb26ff795a9851c457554014
ridwan23 <ridwansantos23@gmail.com>
2018-11-26 20:35:40 +0700

    - first commit - add toast util

---
+++ crocodicrepo/src/main/java/com/crocodic/crocodicrepo/ToastUtil.kt
@@ -0,0 +1,39 @@
+package com.crocodic.crocodicrepo
+
+import android.content.Context
+import android.widget.Toast
+
+
+class ToastUtil constructor(val context: Context){
+
+    private lateinit var toast: Toast
+
+     fun showAToast(st: String, duration: Int) {
+        try {
+            toast.view.isShown
+            toast.setText(st)
+        } catch (e: Exception) {
+            toast = Toast.makeText(context, st, duration)
+        }
+
+        toast.show()
+    }
+
+     fun showAToast(st: String) {
+        try {
+            toast.view.isShown
+            toast.setText(st)
+        } catch (e: Exception) {
+            toast = Toast.makeText(context, st, Toast.LENGTH_SHORT)
+        }
+
+        toast.show()
+    }
+
+    fun cancelToast(){
+        if (toast.view.isShown){
+            toast.cancel()
+        }
+    }
+
+}
